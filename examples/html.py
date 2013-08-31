e1b9c507372a832afd90c6b2b4f3bd00c89d38c9
Samy Pessé <samy.pesse@friendco.de>
2013-08-31 22:04:37 +0200

    Add html and template examples

---
+++ examples/html.py
@@ -0,0 +1,20 @@
+# Import glass library
+import glass
+
+# Import app configs
+import configs
+
+app = glass.Application(
+	name="Hello",
+	client_id=configs.CLIENT_ID,
+	client_secret=configs.CLIENT_SECRET
+)
+
+@app.subscriptions.login
+def login(user):
+	print "user : %s" % user.token
+	user.timeline.post(html="Hello <b>World</b>")
+
+if __name__ == '__main__':
+    app.run(port=8080)
+	
---
+++ examples/template.py
@@ -0,0 +1,22 @@
+# Import glass library
+import glass
+
+# Import app configs
+import configs
+
+app = glass.Application(
+	name="Hello",
+	client_id=configs.CLIENT_ID,
+	client_secret=configs.CLIENT_SECRET,
+	template_folder="templates"
+)
+
+@app.subscriptions.login
+def login(user):
+	print "user : %s" % user.token
+	profile = user.profile()
+	user.timeline.post_template("hello.html", name=profile.get("given_name"))
+
+if __name__ == '__main__':
+    app.run(port=8080)
+	
