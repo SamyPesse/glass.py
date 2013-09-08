442276a01e691373c5ba81f96668677775056080
Samy Pessé <samy.pesse@friendco.de>
2013-09-08 17:58:27 +0200

    Remove apps settings (and the apps from the console)

--- examples/foursquare/config.py
+++ examples/foursquare/config.py
@@ -3,8 +3,8 @@ HOST = "localhost"
 PORT = 5000
 
 # Google
-GOOGLE_CLIENT_ID = "1052883900395.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "rfYSj8_Y9XevHUbFAFfK-Q5y"
+GOOGLE_CLIENT_ID = ""
+GOOGLE_CLIENT_SECRET = ""
 GOOGLE_SCOPES = [
     'https://www.googleapis.com/auth/glass.location',
     'https://www.googleapis.com/auth/glass.timeline',
@@ -13,6 +13,6 @@ GOOGLE_SCOPES = [
 ]
 
 # Foursquare
-FOURSQUARE_CLIENT_ID = "QG0XUORRDRH1H4S0R2XITZTLYLGPV30F13OYAXAZE20Y5DEX"
-FOURSQUARE_CLIENT_SECRET = "B34IC0NTD3GNW2LT3VW2GCWZRLFVFPO1DRC1MAR2WUN1FXPR"
+FOURSQUARE_CLIENT_ID = ""
+FOURSQUARE_CLIENT_SECRET = ""
 FOURSQUARE_CLIENT_REDIRECT = "http://%s:%i/foursquare/callback" % (HOST, PORT)
